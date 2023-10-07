645ca5d65a68d321695f3326067c23f4f798aa13
Mysto <120472333+Mysto01@users.noreply.github.com>
2023-10-07 15:28:42 +1100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mysto'
+title 'mysto_blockdoubleclutch'
+description 'A simple script to disable double clutching for ALL SERVERS (ALL FRAMEWORKS + STANDALONE)'
+
+client_scripts {
+    'client.lua'
+}
